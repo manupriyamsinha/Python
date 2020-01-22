7aef1e1eed0db3462f785c71f4f685442a511606
Manu Priyam Sinha <34571067+manu20sinha@users.noreply.github.com>
2020-01-22 19:37:24 +0530

    Update reverse_num.py

--- reverse_num.py
+++ reverse_num.py
@@ -11,3 +11,18 @@ pass
 n=int(input())
 result = reverse(n)
 print(result)
+
+#-----------2nd method-------
+#revers 2
+def reverse(n):
+    rev = 0
+    while n > 0:
+        a = n % 10
+        rev = (rev * 10) + a
+        n = n // 10
+    return rev
+    
+
+n=int(input())
+result = reverse(n)
+print(result)
